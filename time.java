e5a6a8015be378dd52ccea31be50ad3cc04e0e3f
prakashasus62-star <prakashasus62@gamil.com>
2026-07-20 21:00:00 +0530

    Update Time.java

--- time.java
+++ time.java
@@ -1,18 +1,25 @@
 import java.util.*;
-
-public class Main {
+import java.time.LocalTime;
+import java.time.Duration;
+public class Time {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner d1=new Scanner(System.in);
+        String start=d1.nextLine();
+        String end=d1.nextLine();
+        LocalTime startime=LocalTime.parse(start);
+        LocalTime endTime=LocalTime.parse(end);
+        Duration duration=Duration.between(startime, endTime);
+        Long hours=duration.toHours();
+        Long mini=duration.toMinutes();
+        System.out.println(hours);
+        System.out.println(mini);
+        d1.close();
 
-        String time = sc.next();
-        String[] t = time.split(":");
 
-        int hour = Integer.parseInt(t[0]);
 
-        if (hour >= 12) {
-            System.out.println("PM");
-        } else {
-            System.out.println("AM");
-        }
+        
+
+
     }
+    
 }
