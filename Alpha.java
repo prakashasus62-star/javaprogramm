076ee50fa0e6a6bb51a9eac29f859a3857d84b9e
prakash-stack <prakashasus62@gmail.com>
2026-07-25 13:55:05 +0530

    Add Main class to check if character is alphabetic

---
+++ Alpha.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char ch = sc.next().charAt(0);
+
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
