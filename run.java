97ad78919d77af384a6d4b3a3eb90a5c7546b08f
prakash-stack <prakashasus62@gmail.com>
2026-07-31 21:40:57 +0530

    Add RunningSum class to calculate running totals

---
+++ run.java
@@ -0,0 +1,14 @@
+public class RunningSum {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4};
+
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] = arr[i] + arr[i - 1];
+        }
+
+        for (int num : arr)
+            System.out.print(num + " ");
+    }
+}
